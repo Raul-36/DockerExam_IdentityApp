5bbbdf2a5b2d5d3b9e1c8b99ab4601a3f59fddbb
Step Guest <stepguest@ad.itstep.az>
2024-09-10 21:03:08 +0400

    Add rabbitmq

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using DockerExam_IdentityApp.Dtos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 
 namespace DockerExam_IdentityApp.Controllers
 {
@@ -28,14 +31,44 @@ namespace DockerExam_IdentityApp.Controllers
                 Email = dto.Email,
                 UserName = dto.Name,
             }, dto.Password);
-
-            return result.Succeeded
-                ? base.RedirectToAction(actionName: "Login", controllerName: "Identity")
-                : base.BadRequest(string.Join("\n", result.Errors.Select(error => error.Description)));
+            if(result.Succeeded){
+                PushMessage(JsonSerializer.Serialize(result),"usersQueue");
+                return base.Ok();
+            }
+            else{
+                return base.BadRequest(string.Join("\n", result.Errors.Select(error => error.Description)));
+            }
         }
 
+        void PushMessage(string message, string queueName)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "rabbitmq_app",
+                UserName = "rmuser",
+                Password = "rmpassword"
+            };
 
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
 
+            var result = channel.QueueDeclare(
+                queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false
+            );
 
+            var messageInBytes = Encoding.ASCII.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: queueName,
+                basicProperties: null,
+                body: messageInBytes
+            );
+
+            System.Console.WriteLine($"Push: '{message}'");
+        }
     }
 }
